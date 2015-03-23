374d62c24d11995800ab1391292f6f6d57d4e0d2
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-22 22:25:15 -0700

    :new: Add core-ally-keys

---
+++ Sources/Base/core-a11y-keys.hh
@@ -0,0 +1,8 @@
+<?hh //strict
+class :core-a11y-keys extends :polymer-xhp{
+  attribute
+    Stringish keys,
+    Stringish target;
+  children empty;
+  protected string $tagName = 'core-ally-keys';
+}
