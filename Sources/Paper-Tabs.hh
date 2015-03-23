11baf3c38133f727393c98eef09e919d37aa0cf1
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-22 17:34:06 -0700

    :new: Add Paper-Tabs

---
+++ Sources/Paper-Tabs.hh
@@ -0,0 +1,13 @@
+<?hh //strict
+class :paper-tabs extends :core-selector{
+  attribute
+    bool noink,
+    bool nobar,
+    bool noslide,
+    bool scrollable,
+    bool disableDrag,
+    bool hideScrollButton,
+    bool alignBottom;
+  children (:paper-tab);
+  protected string $tagName = 'paper-tabs';
+}
