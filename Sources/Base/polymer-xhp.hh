fffcfd24901cd0bd0613c3d757782b3070d233a8
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-22 22:27:21 -0700

    :new: Add polymer-xhp

---
+++ Sources/Base/polymer-xhp.hh
@@ -0,0 +1,28 @@
+<?hh //strict
+class :polymer-xhp extends :xhp:html-element{
+  attribute
+    bool layout,
+    bool horizontal,
+    bool vertical,
+    bool flex,
+    bool unresolved,
+    bool two,
+    bool three,
+    bool auto-vertical,
+    bool center,
+    bool start,
+    bool end,
+    bool start-justified,
+    bool center-justified,
+    bool end-justified,
+    bool justified,
+    bool around-justified,
+    bool reverse,
+    bool fullbleed,
+    bool block,
+    bool hidden,
+    bool relative,
+    bool fit,
+
+    Stringish touch-action;
+}
